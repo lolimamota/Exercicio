5b14d0064b11743b55db1fd7c1a0587e63aa5732
Lorrayne <lorraynelimamota@gmail.com>
2024-03-05 14:16:20 -0300

    estilização

--- exercicio-vnw/src/Components/Footer/Footer.jsx
+++ exercicio-vnw/src/Components/Footer/Footer.jsx
@@ -1,5 +1,31 @@
-import React, {useState} from 'react'
-import { useEffect } from 'react'
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section`
+display:flex;
+justify-content: space-around;
+flex-direction: column;
+align-items: center;
+font-size: 1.5rem;
+font-family: "Honk", system-ui;
+height: 20vh;
+`;
+
+const Div = styled.div`
+width: 50vw;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+font-size: 1.5rem;
+`;
+
+const Button = styled.button`
+width: 10vw;
+height: 5vh;
+background-color: white;
+color: black;
+border: solid white;
+border-radius: 10px;`;
 
 function Footer() {
 const [time, setTime] = useState(0)
@@ -16,14 +42,14 @@ const [keep, setKeep] = useState()
   const End = ()=> {clearInterval(keep)};
 
   return (
-    <section>
+    <Section>
       <h2>Tempo que fiquei preso nessa página:</h2>
       <h4>{time}</h4>
-      <div>
-        <button>PARAR</button>
-        <button className="display" onClick={End}>Desculpa!</button>
-      </div>
-    </section>
+      <Div>
+        <Button>PARAR</Button>
+        <Button className="display" onClick={End}>Desculpa!</Button>
+      </Div>
+    </Section>
   )
 }
 
